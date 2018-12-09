c5f88827f05eebd82b0d644664ab4dcc0dd5b2b9
up864163 <up864163@myport.ac.uk>
2018-12-09 01:06:32 +0000

    Added colour changing on progress

--- infinity.lua
+++ infinity.lua
@@ -3,15 +3,35 @@
 	Description: Draw an infinity symbol using dxDrawLine3D, using only the Z and Y axis.
 --]]
 
+--[[ UTILITY FUNCTION ]]
+function getRGColorFromPercentage(percentage)
+	if not percentage or
+		percentage and type(percentage) ~= "number" or
+		percentage > 100 or percentage < 0 then
+		outputDebugString( "Invalid argument @ 'getRGColorFromPercentage'", 2 )
+		return false
+	end
+
+	if percentage > 50 then
+		local temp = 100 - percentage
+		return temp*5.1, 255, 0
+	elseif percentage == 50 then
+		return 255, 255, 0
+	end
+	
+	return 255, percentage*5.1, 0
+end
+
 --[[ 
      	Each section needs be drawn to look continous.
      	To achieve this, I draw each quarter separately.
         Here is a table with the domain ranges for the polar equation.
 --]]
 
-local SCALE_SIZE = 500
+local SCALE_SIZE = 250
 local MAX_POINTS = 10
 local INTERVAL = 0.1
+
 local ranges = {
 	{1 * math.pi, 1.5 * math.pi}, -- Forwards
 	{0.5 * math.pi, 0}, -- Backwards
@@ -43,7 +63,7 @@ function removePoint()
 end
 
 function startNewSection()
-	lastSectionDrawn = lastSectionDrawn + 1
+		lastSectionDrawn = lastSectionDrawn + 1
 		-- Wrap back to the start
 		if lastSectionDrawn > 4 then 
 			lastSectionDrawn = nil
@@ -90,11 +110,8 @@ function addPoint()
 
 	if validate(x, y) then
 		points[#points + 1] = {x, y}
-	else
-		return false
 	end
 		
-	
 end
 
 function validate(...) 
@@ -113,14 +130,14 @@ end
 
 local screenW, screenH = guiGetScreenSize()
 --[[ Draw all of the points ]]
-function drawPoints()
+function drawPoints(r, g, b, width)
 	if #points < 2 then return false end	
 	for i = 2, #points do
 		local y1, z1 = points[i-1][1], points[i-1][2]
 		local y2, z2 = points[i][1], points[i][2]
 		
 		if validate(y1, z1, y2, z2) then
-			dxDrawLine(screenW/2 + y1, screenH/2 + z1, screenW/2 + y2, screenH/2 + z2)
+			dxDrawLine(screenW/2 + y1, screenH/2 + z1, screenW/2 + y2, screenH/2 + z2, tocolor(r, g, b), width)
 		end
 	end
 end
@@ -137,8 +154,17 @@ addEventHandler("onClientPreRender", getRootElement(), addInfinity)
 
 function drawInfinity()
 	if not start then return false end
-	drawPoints()
-	if (getTickCount() - start > maxWait) then start = nil end
+	
+	local elapsedTime = getTickCount() - start
+	local progress = math.min(elapsedTime / maxWait, 1)
+	local width = 5
+	local r, g, b = getRGColorFromPercentage(math.ceil(progress * 100))
+	drawPoints(r, g, b, width)	
+
+	if (progress >= 1) then 
+		start = nil 
+		points = {}	-- Empty the list 
+	end
 end
 addEventHandler("onClientRender", getRootElement(), drawInfinity)
 
